b749c8f9cf207a1dafea1d9df7cdcb2dbbb9bd3b
Han Min Thaw <hanminthaw15@gmail.com>
2026-02-20 16:44:30 +0700

    add models

---
+++ app/Models/Accessory.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Accessory extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['asset_id', 'name'];
+
+    public function asset()
+    {
+        return $this->belongsTo(Asset::class);
+    }
+}
