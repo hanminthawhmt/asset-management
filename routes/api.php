baa8bf7e2ab4188525a2251237a0335c599d4e0c
Han Min Thaw <hanminthaw15@gmail.com>
2026-02-23 14:15:28 +0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ApproverController;
 use App\Http\Controllers\AssetController;
 use App\Http\Controllers\AssetTypeController;
 use App\Http\Controllers\DefectController;
+use App\Http\Controllers\FormOptionController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,8 +15,17 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::resource('asset-types', AssetTypeController::class);
+
 Route::resource('assets', AssetController::class);
+
 Route::resource('accessories', AccessoryController::class);
+
 Route::resource('defects', DefectController::class);
+
 Route::resource('transactions', TransactionController::class);
+Route::patch('transactions/{transaction}/approve', [TransactionController::class, 'approve']);
+Route::patch('transactions/{transaction}/reject', [TransactionController::class, 'reject']);
+
 Route::resource('approvers', ApproverController::class);
+
+Route::get('form-options', [FormOptionController::class, 'getPage']);
